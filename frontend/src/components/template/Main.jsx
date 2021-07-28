652076ad7e5bddb388f22087624df16079864610
Roger Lopes <roger@dzigual.com.br>
2021-07-28 18:28:15 -0300

    Primeiro Commit

---
+++ frontend/src/components/template/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+import Header from './Header'
+
+export default props =>
+  <React.Fragment>
+    <Header />
+    <main className="content">
+      Conteúdo
+    </main>
+  </React.Fragment>
+
---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+export default props =>
+  <div className="app">
+    <Logo />
+    <Nav />
+    <Main />
+    <Footer />
+  </div>
